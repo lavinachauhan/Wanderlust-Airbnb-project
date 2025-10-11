608a6b8ed6cd0b09bc27b1c00e860159e1d19249
lavinachauhan <chauhanlavi1627@gmail.com>
2025-10-11 21:41:21 +0530

    index and show route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require ("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main()
@@ -11,9 +12,15 @@ main()
 .catch((err) => {
     console.log(err);
 })
+
 async function main(){
     await mongoose.connect(MONGO_URL);
 }
+
+app.set("views engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended : true}));
+
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
@@ -30,6 +37,22 @@ app.get("/testListing", async (req, res) => {
     await sampleListing.save();
     console.log("Sample was saved");
     res.send("Successful testing");
+})
+
+// index route
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    // console.log(allListings);
+    res.render("listings/index.ejs", {allListings});
+})
+
+// show route
+app.get("/listings/:id", async (req, res) => {
+    let {id}= req.params;
+    const detail = await Listing.findOne({_id : id});
+    // console.log("Details printed")
+    // console.log(detail);
+    res.render("listings/show.ejs", {detail});
 })
 
 app.listen(8080, () => {
